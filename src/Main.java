8bde073f047876cff6d095fd374b29d53dc5ac0a
my.h.m@yandex.ru <Zxcv7788jetbrains>
2022-05-30 13:10:05 +0300

    Массивы №2

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,8 @@ public class Main {
 
     public static void main(String[] args) {
         int[] weights = {90, 91, 93, 92, 85, 87, 88, 89, 0, 0, 0, 0};
-        weights[0] = 90;
+        int[] weightsCopy = {902, 91, 93, 92, 85, 87, 88, 89, 0, 0, 0, 0};
+        weightsCopy[0] = 823;
         int januaryWeight = weights[0];
         System.out.println(januaryWeight);
         System.out.println(weights[0]);
@@ -13,6 +14,27 @@ public class Main {
         for (int i = 0; i < weights.length; i++) {
             System.out.println(weights[i]);
 
+        }
+        boolean arraysAreEqual = true;
+        arraysAreEqual = weights.length == weightsCopy.length;
+        if (arraysAreEqual) {
+            for (int i = 0; i < weights.length; i++) {
+                if (weights[i] != weightsCopy[i]) {
+                    arraysAreEqual = false;
+                }
+            }
+        }
+        if (weights == weights) {
+            System.out.println("Массивы одинаковы");
+        } else {
+            System.out.println("Массивы разные");
+        }
+        int maxWeight = -1;
+        for (int weight : weights) {
+            if (weight > maxWeight) {
+                maxWeight = weight;
+                System.out.println(maxWeight);
+            }
         }
     }
 }
